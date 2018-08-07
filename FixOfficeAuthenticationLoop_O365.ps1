cb4b593f4cb016b9ac0268b9b1a3eefd580e3481
DBCI-David <42154648+DBCI-David@users.noreply.github.com>
2018-08-07 13:25:55 +0200

    Add files via upload

---
+++ FixOfficeAuthenticationLoop_O365.ps1
@@ -0,0 +1,19 @@
+﻿<#ScriptName:
+ScriptDate: 
+Set the regkey to bypass endless authentication loop (got from Microsoft support)
+David Blum conseils informatiques - david.blum@db-conseilsinformatiques.ch
+
+[HKEY_CURRENT_USER\Software\Microsoft\Office\16.0\Common\Identity] 
+
+Add the Dword as : "DisableADALatopWAMOverride"=dword:00000001
+
+Restart your computer and check the outcome.
+
+
+
+#>
+
+Push-Location
+Set-Location HKCU:
+Test-Path .\Software\Microsoft\Office\16.0\Common\Identity\
+New-Item -Path HKCU:\Software\Microsoft\Office\16.0\Common\Identity\ -Name "DisableADALatopWAMOverride" -Value 1 -Type "DWord"
---
+++ FixWindows10_WOL_enable.ps1
@@ -0,0 +1,21 @@
+﻿<#ScriptName:
+ScriptDate: 
+Set the regkey to bypass endless authentication loop (got from Microsoft support)
+David Blum conseils informatiques - david.blum@db-conseilsinformatiques.ch
+
+[HKEY_CURRENT_USER\Software\Microsoft\Office\16.0\Common\Identity] 
+
+Add the Dword as : "DisableADALatopWAMOverride"=dword:00000001
+
+Restart your computer and check the outcome.
+
+
+
+#>
+
+Push-Location
+Set-Location HKLM:
+Test-Path .\SYSTEM\CurrentControlSet\Control\Session Manager\Power 
+New-Item -Path HKCU:\Software\Microsoft\Office\16.0\Common\Identity\ -Name "HiberBootEnabled" -Value 0 -Type "DWord"
+
+
